d361f8a25c96bb2af995d233fae16cc57191d67d
Tomohiro Kawaguchi <tomohiro@Tomohiro-no-MacBook-Air.local>
2019-04-13 16:39:18 +0900

    構造修正

--- kabu-analysis.py
+++ kabu-analysis.py
@@ -1,5 +1,61 @@
+from kabu import StockDB
+
+class StockDB_Analyzer(StockDB):
+    def __init__(self):
+        StockDB.__init__(self)
+        self._rsi = self._db['RSI']
+    def updateRSI(self):
+        codes = [x['Code'] for x in self._codes.find()]
+        self._db.begin()
+        for code in codes:
+            print(code)
+            for data in self._prices.find(Code=code,order_by='-Date',_limit=100):
+                date = data['Date']
+                rsi = self.RSI(code,date)
+                record = {
+                'Code':code,
+                'Date':date,
+                'RSI':rsi[0],
+                }
+                self._codes.upsert(record,['Code','Date'])
+        self._db.commit()
+
+    def Average(self,code,date):
+        ret = []
+        for term in [5,25,75]:
+            result = self._prices.find(
+            Code=code,
+            Date={'<=':date},
+            order_by='-Date',
+            _limit=term,
+            )
+            data = [x['End'] for x in result]
+            ret.append(float(sum(data))/float(len(data)))
+        return ret
+
+    def RSI(self,code,date):
+        ret = []
+        for term in [14]:
+            result = self._prices.find(
+            Code=code,
+            Date={'<=':date},
+            order_by='-Date',
+            _limit=term+1,
+            )
+            data = [x['End'] for x in result]
+            diff = list(map(lambda x:x[0]-x[1],zip(data,data[1:])))
+            a = list(filter(lambda x:x>0.,diff))
+            b = list(filter(lambda x:x<0.,diff))
+            try:
+                rsi = 100.*sum(a)/(sum(a)-sum(b))
+            except:
+                rsi = 50.0
+                print(code,date,data,diff)
+            ret.append(rsi)
+
+        return ret
+
 if __name__ == '__main__':
-    from kabu import StockDB_Analyzer
     import datetime
     today = int('{0:%Y%m%d}'.format(datetime.date.today()))
 
@@ -13,9 +69,4 @@ if __name__ == '__main__':
     print(args)
 
     a=StockDB_Analyzer()
-    for code in args.codes:
-        print('Code:',code)
-        if args.rsi:
-            print('RSI: ',a.RSI(code,args.date))
-        if args.average:
-            print('Ave: ',a.Average(code,args.date))
+    a.updateRSI()
