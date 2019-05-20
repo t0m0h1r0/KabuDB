8592367b76969508a5fd374586704f426cc264a3
Tomohiro Kawaguchi <tomohiro@Tomohiro-no-MacBook-Air.local>
2019-05-20 21:35:37 +0900

    隠れ層追加

--- predict.py
+++ predict.py
@@ -15,15 +15,15 @@ class Kabu:
     def __init__(self,filename='^N225.csv'):
         self._data =[]
         self._filename = filename
-        self._config = {'keep':10,'term':25,'change':0.03}
-        self._ml = {'hidden':300,}
+        self._config = {'keep':10,'term':25,'change':0.025}
+        self._ml = {'hidden':2000,'epoch':100,'batch':5000}
         self._x = []
         self._y = []
         self._z = []
 
     def _read(self):
         df = pd.read_csv(self._filename,index_col=0)
-        self._data = np.log(df.dropna(how='any')[df.Volume>0.])[-2000:]
+        self._data = np.log(df.dropna(how='any')[df.Volume>0.])[-1000:]
 
     def _save(self):
         with open(self._filename+'.json','w') as f:
@@ -43,6 +43,39 @@ class Kabu:
 
         #print( np.log(f_data[0:1]).values[0] )
         return f_data[0:1].values[0]
+
+    def _trade4(self,data):
+        f_data = data.reset_index(drop=True)
+
+        #翌日購入
+        buy = f_data.at[1,'Open']
+        #最大last日後まで保有
+        last = self._config['keep']+1
+        #最低利益
+        threshold = self._config['change']+1.
+
+        category = np.zeros(last)
+        #最終日前日でなく、翌日値上がり期待するなら売らない
+        #最終日前日でなく、翌日値上がり期待できないなら売る
+        for day in range(2,last-1):
+            sell = f_data.at[day,'Open']
+            keep = f_data.at[1+day,'Open']
+            if buy + np.log(threshold) < sell and sell > keep:
+                category[day-1]=1
+                break
+        #最終日前日なら売る
+        else:
+            sell = f_data.at[last-1,'Open']
+            if buy + np.log(threshold) < sell:
+                category[last-2]=1
+            #買ったら損する
+            elif buy > sell:
+                category[last-1]=1
+            #買わないほうがいい
+            else:
+                category[0]=1
+        assert sum(category)==1, '条件漏れ'
+        return category
 
     def _trade3(self,data):
         f_data = data.reset_index(drop=True)
@@ -135,7 +168,7 @@ class Kabu:
             data = self._series(self._data[k-term:k])
             dataset.append(data)
         for k in range(term,k_end-keep-1):
-            data = self._trade(self._data[k:k+keep+1])
+            data = self._trade4(self._data[k:k+keep+1])
             label.append(data)
 
         x = np.reshape(scaler.fit_transform(dataset),
@@ -155,12 +188,14 @@ class Kabu:
             return_sequences=False,
             activation='relu',
             batch_input_shape=(None, days, dimension)))
-        model.add(Dense(75))
+        #model.add(Dense(75))
         #model.add(Dense(25))
+        #model.add(Dense(73))
+        #model.add(Dense(23))
         model.add(Dense(len(self._y[0]),activation='softmax'))
         optimizer = Adam(lr=0.001)
         model.compile(loss="mean_squared_error", optimizer=optimizer, metrics=['accuracy'])
-        model.fit(self._x, self._y, epochs=100, batch_size=512, validation_split=0.2)
+        model.fit(self._x, self._y, epochs=self._ml['epoch'], batch_size=self._ml['batch'], validation_split=0.2)
         self._model = model
 
     def _predict(self):
