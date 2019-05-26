617f4da81498b5113e229d0628d9888c15764c36
Tomohiro Kawaguchi <tomohiro@Tomohiro-no-MacBook-Air.local>
2019-05-26 15:06:16 +0900

    print文修正

--- predict.py
+++ predict.py
@@ -78,7 +78,7 @@ class Kabu:
             diff.append(sell-buy)
         diff = np.sort(np.array(diff),axis=0)
         separator = [diff[x*int(len(data)/counts)-1] for x in range(counts-1)]
-        print(np.exp(1.+separator))
+        print(np.exp(1.+np.array(separator)))
 
         output = []
         for k in data.index:
