d324dc7c2e75c0058af0827508def553b85dc232
Tomohiro Kawaguchi <tomohiro@Tomohiro-no-MacBook-Air.local>
2019-06-03 01:16:11 +0900

    pritコメントアウト

--- predict.py
+++ predict.py
@@ -136,9 +136,6 @@ class Kabu:
         #print(wave)
 
         self._y = label.values
-        np.set_printoptions(formatter={'float': '{: 0.5f}'.format})
-        print(self._y)
-        #print(self._scaler.inverse_transform(self._y))
         self._x,self._z = np.split(dataset,[len(self._y)])
         self._wx,self._wz = np.split(wave,[len(self._y)])
 
