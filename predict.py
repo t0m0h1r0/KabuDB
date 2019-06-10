5f9ab1b0f3e287c2407ed49ba730ea160a2fc564
Tomohiro Kawaguchi <tomohiro@Tomohiro-no-MacBook-Air.local>
2019-06-10 14:25:45 +0900

    .

--- predict.py
+++ predict.py
@@ -160,7 +160,7 @@ class KabuQRNN:
             callbacks=[early_stopping])
 
     def _predict(self,model,data):
-        _data = data[-self.config['term']:]
+        _data = data[-self._config['term']:]
         for x in range(self._config['predict']):
             x,y,z = self._generate(_data)
             y = model.predict(z)
