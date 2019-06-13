5b3f590f6bfe1aa3233944cb2cbaac9beeff3de0
Tomohiro Kawaguchi <tomohiro@Tomohiro-no-MacBook-Air.local>
2019-06-14 00:41:42 +0900

    .

--- predict.py
+++ predict.py
@@ -114,7 +114,7 @@ class KabuQRNN:
         hidden = trial.suggest_int('hidden',64,256)
         dropout_rate = trial.suggest_uniform('dropout_rate',0,1)
         activation = trial.suggest_categorical('activation',['sigmoid','relu'])
-        optimizer = trial.suggest_categorical('optimizer', ['sgd', 'adam', 'rmsprop'])
+        optimizer = trial.suggest_categorical('optimizer', ['sgd', 'adam', 'rmsprop', 'adamax', 'nadam'])
 
         model, base = self._build(
             layers=[layer_r,layer_w],
